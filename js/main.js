ea9034dc5f5140d76304b2e6ce7fb543f9b39565
Lê Thanh Phương <98444115+Kouro2510@users.noreply.github.com>
2023-03-17 11:51:58 +0700

    Compele template

--- js/main.js
+++ js/main.js
@@ -12,4 +12,63 @@ $(window).on("load", function () {
         setTimeout(slideShow,5000)
     }
     slideShow()
+
+    const wHeight = $(window).height();
+    $(".gallery-popup img").css("max-height",wHeight + "px" );
+    let itemIndex=0;
+    const totalGalleryItem= $(".gallery-item").length;
+    $(".gallery-item").click(function (){
+        itemIndex=$(this).index();
+        $(".gallery-popup").addClass("open")
+        $(".gallery-popup .gp-img").hide()
+        gpSlideShow()
+    })
+    $(".gp-controls .next").click(function (){
+        if(itemIndex === totalGalleryItem-1){
+            itemIndex=0;
+        }
+        else {
+            itemIndex++;
+        }
+        $(".gallery-popup .gp-img").fadeOut(function (){
+            gpSlideShow();
+        })
+    })
+    $(".gp-controls .prev").click(function (){
+        if(itemIndex === 0){
+            itemIndex=totalGalleryItem-1;
+        }
+        else {
+            itemIndex--;
+        }
+        $(".gallery-popup .gp-img").fadeOut(function (){
+            gpSlideShow();
+        })
+    })
+    function gpSlideShow(){
+        const imgSrc= $(".gallery-item").eq(itemIndex).find("img").attr("data-large");
+        $(".gallery-popup .gp-img").fadeIn().attr("src",imgSrc);
+        $(".gp-counter").text((itemIndex+1) +"/"+ totalGalleryItem)
+    }
+    $(".gp-close").click(function (){
+        $(".gallery-popup").removeClass("open");
+    })
+    $(".gallery-popup").click(function (event){
+        if($(event.target).hasClass("open")){
+            $(".gallery-popup").removeClass("open");
+        }
+    })
+})
+$(document).ready(function (){
+    $(window).scroll(function (){
+        if($(this).scrollTop()>100){
+            $(".header").addClass("fixed")
+        }
+        else {
+            $(".header").removeClass("fixed")
+        }
+    })
+    $.scrollIt({
+        topOffset: -50
+    });
 })
